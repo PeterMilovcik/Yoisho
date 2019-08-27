70905f7f4b0fcbf271e2e246b3e24f9aae3c9e7f
Peter Milovcik <peter.milovcik@gmail.com>
2019-08-27 20:33:38 +0200

    Initial version

---
+++ Yoisho/Yoisho/Yoisho/AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Yoisho
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
